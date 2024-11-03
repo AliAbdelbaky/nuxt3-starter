1044d7d714fb1160dad3bd8404b9a4aefa552670
Ali Abdelbaqy <61080704+AliAbdelbaky@users.noreply.github.com>
2024-11-03 15:21:46 +0200

    Update deps (#15)
    * update deps
    
    * fix: unresolved imports

--- composables/core/useNotificationHandler.ts
+++ composables/core/useNotificationHandler.ts
@@ -2,21 +2,17 @@ import useNThemeHandler from "~/composables/core/useNThemeHandler";
 import {
     createDiscreteApi,
     type NotificationOptions,
-    type NotificationPlacement
+    type NotificationPlacement,
+    type NotificationApi
 
 } from "naive-ui";
 
 export default function (placement?: NotificationPlacement) {
     const {theme_overrides} = useNThemeHandler()
-    const {notification} = createDiscreteApi(
-        ['message', 'dialog', 'notification', 'loadingBar', 'modal'],
-        {
-            configProviderProps: {themeOverrides: theme_overrides},
-            notificationProviderProps: {placement: placement || 'top-right'}
-        }
-    )
+    const notification = ref<NotificationApi | null>(null)
+
     const notify = (payload: NotificationOptions) => {
-        notification.create({
+        notification.value?.create({
             ...payload,
             duration: 2500,
             keepAliveOnHover: true
@@ -25,6 +21,17 @@ export default function (placement?: NotificationPlacement) {
     const getErrorMsg = (error: any) => {
         return error?.response?.data?.message || error?.message || 'An error occurred'
     }
+    onMounted(() => {
+        if (notification.value) return
+        const _createDiscreteApi = createDiscreteApi(
+            ['message', 'dialog', 'notification', 'loadingBar', 'modal'],
+            {
+                configProviderProps: {themeOverrides: theme_overrides},
+                notificationProviderProps: {placement: placement || 'top-right'}
+            }
+        )
+        notification.value = _createDiscreteApi.notification
+    })
     return {
         notify,
         getErrorMsg
